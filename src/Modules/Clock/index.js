f294c015d6d11c406454ca1d50af204dc4d0e1ca
Mickey <develop.agility@gmail.com>
2016-03-07 20:20:48 -0700

    clock housekeeping

--- src/Modules/Clock/index.js
+++ src/Modules/Clock/index.js
@@ -7,17 +7,11 @@ export default class Clock extends React.Component {
     this.state = {
       AMPM: this.getAMPM(),
       hours: ((this.date.getHours() + 11) % 12 + 1),
-      minutes: this.addMinZero(this.date.getMinutes()),
-      seconds: this.addSecZero(this.date.getSeconds())
+      minutes: this.addZero(this.date.getMinutes()),
+      seconds: this.addZero(this.date.getSeconds())
     };
   }
-  addMinZero(value){
-    if(value < 10)
-      return '0'+ value;
-    else
-      return value
-  }
-  addSecZero(value){
+  addZero(value){
     if(value < 10)
       return '0'+ value;
     else
@@ -34,8 +28,8 @@ export default class Clock extends React.Component {
     this.setState({
       AMPM: this.getAMPM(),
       hours: ((this.date.getHours() + 11) % 12 + 1),
-      minutes: this.addMinZero(this.date.getMinutes()),
-      seconds: this.addSecZero(this.date.getSeconds())
+      minutes: this.addZero(this.date.getMinutes()),
+      seconds: this.addZero(this.date.getSeconds())
     });
   }
   componentDidMount(){
